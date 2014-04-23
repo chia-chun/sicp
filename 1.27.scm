64b78a9a5fa62ef234a71dfa81834681ddd6cff0
chia-chun <chia-chun@users.noreply.github.com>
2014-04-23 09:42:33 +0800

    1.27

---
+++ 1.27.scm
@@ -0,0 +1,51 @@
+(define (square x)
+  (* x x))
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+(define (fermat-test-all n)
+  (define (test n a)
+    (cond ((= n a) #t)
+          ((= (expmod a n n) a) (test n (+ a 1)))
+          (else #f)))
+  (test n 1))
+
+(define (fermat-test n)
+  (define (try-it a)
+     (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) #t)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else #f)))
+
+(define (runtime)
+  (+ (* 1000000 (car (gettimeofday))) (cdr (gettimeofday))))
+
+(define (timed-prime-test n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (fast-prime? n 10)
+      (report-prime n (- (runtime) start-time))))
+
+(define (report-prime n  elapsed-time)
+  (newline)
+  (display n)
+  (display "***")
+  (display elapsed-time))
+
+(define (search-for-primes a b)
+  (cond ((< b a) (newline) (display "End") (newline))
+        ((= b a) (timed-prime-test a))
+        ((= (remainder a 2) 0) (search-for-primes (+ 1 a) b))
+        (else (timed-prime-test a)
+              (search-for-primes (+ 2 a) b))))
