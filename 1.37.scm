66863b781497db506f62fb73420bde9b3155a64a
chiachun <chia-chun@users.noreply.github.com>
2014-05-15 23:33:08 +0800

    1.37

---
+++ 1.37.scm
@@ -0,0 +1,22 @@
+(define (cont-frac n d k)
+  (define (calc x)
+    (if (= x k)
+        (/ (n x) (d x))
+        (/ (n x) (+ (d x) (calc (+ x 1))))))
+  (calc 1))
+
+(define (cont-frac-iter n d k)
+  (define (iter count result)
+    (let ((x result))
+       (if (= count 1)
+           result
+           (iter (- count 1) (/ (n (- count 1)) (+ (d (- count 1)) x))))))
+  (iter k (/ (n k) (d k))))
+
+(cont-frac (lambda (i) 1.0)
+           (lambda (i) 1.0)
+           10)
+
+(cont-frac-iter (lambda (i) 1.0)
+                (lambda (i) 1.0)
+                10)
