8932541f269f15a77e0a2d2dde3d2b0fb54649b0
chiachun <chia-chun@users.noreply.github.com>
2014-05-16 21:13:21 +0800

    1.38

--- 1.38.scm
+++ 1.38.scm
@@ -1,14 +1,15 @@
 (define (cont-frac n d k)
-  (define (calc x)
-    (if (= x k)
-        (/ (n x) (d x))
-        (/ (n x) (+ (d x) (calc (+ x 1))))))
-  (calc 1))
-
-(define (cont-frac-iter n d k)
   (define (iter count result)
     (let ((x result))
        (if (= count 1)
            result
            (iter (- count 1) (/ (n (- count 1)) (+ (d (- count 1)) x))))))
   (iter k (/ (n k) (d k))))
+
+(define (approx-e k)
+  (define (n i) 1.0)
+  (define (d i)
+     (if (= (remainder i 3) 2)
+         (+ 2 (* 2 (/ (- i 2) 3)))
+         1.0))
+  (+ 2 (cont-frac n d k)))
