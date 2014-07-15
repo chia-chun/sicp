21c0de841113c3b529c84402a41ddc90145c5f8c
chia-chun <chia-chun@users.noreply.github.com>
2014-07-15 11:51:24 +0800

    2.28

--- 2.28.scm
+++ 2.28.scm
@@ -1,16 +1,9 @@
 (define (fringe items)
   (cond ((not (pair? items)) items)
-        ((null? (cdr items)) (fringe (car items)))
-        (else (append (list (car items)) (fringe (car (cdr items)))))))
+        ((not (pair? (car items))) (cons (car items) (fringe (cdr items))))
+        (else (append (fringe (car items)) (fringe (cdr items))))))
 
 (define (append list1 list2)
   (if (null? list1)
       list2
       (cons (car list1) (append (cdr list1) list2))))
-
-(define (fringe2 items)
-  (let ((result 1))
-    (define (fringe-iter li)
-      (cond ((not (pair? li)) (define result (cons li result)))
-            (else (fringe-iter (car li)) (fringe-iter (cdr li))))
-    (fringe-iter items))))
