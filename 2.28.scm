5180830cc17e9b7586a565244d44302a378893c3
chia-chun <chia-chun@users.noreply.github.com>
2014-07-16 20:31:22 +0800

    2.28-revised

--- 2.28.scm
+++ 2.28.scm
@@ -2,6 +2,11 @@
   (cond ((not (pair? items)) items)
         ((not (pair? (car items))) (cons (car items) (fringe (cdr items))))
         (else (append (fringe (car items)) (fringe (cdr items))))))
+;; revised according to section 2.2.3
+(define (fringe2 items)
+  (cond ((null? items) '())
+        ((not (pair? items)) (list items))
+        (else (append (fringe2 (car items)) (fringe2 (cdr items))))))
 
 (define (append list1 list2)
   (if (null? list1)
