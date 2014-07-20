3ffffa166b1d98546d9579b8ad723a988baccfe2
chia-chun <chia-chun@users.noreply.github.com>
2014-07-20 12:30:25 +0800

    2.40

--- 2.40.scm
+++ 2.40.scm
@@ -1,3 +1,35 @@
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum? (unique-pairs n))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (unique-pairs n)
+  (accumulate append
+              '()
+              (map (lambda (i)
+                     (map (lambda (j) (list i j))
+                          (enumerate-interval 1 (- i 1))))
+                   (enumerate-interval 1 n))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      '()
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (prime? n)
+  (fast-prime? n 100))
+;; The following prime test is from exercise 1.28
 (define (square x)
   (* x x))
 
@@ -34,7 +66,7 @@
   (if (fast-prime? n 100)
       (report-prime n (- (runtime) start-time))))
 
-(define (report-prime n elapsed-time)
+(define (report-prime n  elapsed-time)
   (newline)
   (display n)
   (display "***")
