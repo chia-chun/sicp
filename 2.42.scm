71f18bd313b7312469439535210f066a6f8877fc
chia-chun <chia-chun@users.noreply.github.com>
2014-07-23 11:21:01 +0800

    2.42-final

--- 2.42.scm
+++ 2.42.scm
@@ -2,18 +2,18 @@
 ;; For example, the solution provided in Fig. 2.8 is (3 7 2 8 5 1 4 6).
 (define (queens board-size)
   (define (queen-cols k)
-    ((if (= k 0)
-         (list empty-board)
-         (filter
-           (lambda (positions) (safe? k positions))
-           (flatmap
-             (lambda (rest-of-queens)
-               (map (lambda (new-row)
-                      (adjoin-position new-row
-                                       k
-                                       rest-of-queens))
-                    (enumerate-interval 1 board-size)))
-             (queen-cols (- k 1)))))))
+    (if (= k 0)
+        (list empty-board)
+        (filter
+          (lambda (positions) (safe? k positions))
+          (flatmap
+            (lambda (rest-of-queens)
+              (map (lambda (new-row)
+                     (adjoin-position new-row
+                                      k
+                                      rest-of-queens))
+                   (enumerate-interval 1 board-size)))
+            (queen-cols (- k 1))))))
   (queen-cols board-size))
 
 (define (safe? k positions)
@@ -22,15 +22,16 @@
         (car positions)
         (get (- kth 1) (cdr positions))))
   (define (row-safe? k positions)
-    (cond ((= k 1) #t)
-          ((= (get k positions) (car positions)) #f)
-          (else (row-safe? (- k 1) (cdr positions)))))
+    (if (= k 1)
+        #t
+        (and (not (= (get k positions) (car positions)))
+             (row-safe? (- k 1) (cdr positions)))))
   (define (diagonal-safe? k positions)
     (if (= k 1)
         #t
-        (not (or (= (- k 1) (- (get k positions) (car positions)))
-                 (= (+ (get k positions) (car positions)) (+ k 1))
-                 (diagonal-safe? (- k 1) (cdr positions))))))
+        (and (not (= (- k 1) (- (get k positions) (car positions))))
+             (not (= (- (car positions) (get k positions)) (- k 1)))
+             (diagonal-safe? (- k 1) (cdr positions)))))
   (and (row-safe? k positions) (diagonal-safe? k positions)))
 
 (define (adjoin-position new-row k rest-of-queens)
