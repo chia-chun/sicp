aa2c4fe37ae541e673c4d2eb3cf754b52c100cfd
chia-chun <chia-chun@users.noreply.github.com>
2014-07-29 16:41:51 +0800

    2.54

---
+++ 2.54.scm
@@ -0,0 +1,6 @@
+(define (equal? a b)
+  (cond ((and (null? a) (null? b)) #t)
+        ((and (pair? a) (not (pair? b))) #f)
+        ((and (pair? b) (not (pair? a))) #f)
+        ((not (or (pair? a) (pair? b))) (eq? a b))
+        (else (and (equal? (car a) (car b)) (equal? (cdr a) (cdr b))))))
