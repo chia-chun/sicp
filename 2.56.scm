f47c2d2f4bc8334648f687a9de2678ce8d8a7e0b
chia-chun <chia-chun@users.noreply.github.com>
2014-08-01 21:40:00 +0800

    2.56

--- 2.56.scm
+++ 2.56.scm
@@ -1,3 +1,24 @@
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+          (make-product (multiplier exp)
+                        (deriv (multiplicand exp) var))
+          (make-product (deriv (multiplier exp) var)
+                        (multiplicand exp))))
+        ((exponentiation? exp)
+         (make-product (exponent exp)
+                       (make-product
+                        (make-exponentiation (base exp)
+                                             (- (exponent exp) 1))
+                        (deriv (base exp) var))))
+        (else (error "unknown expression type - DERIV" exp))))
+
 (define (variable? x) (symbol? x))
 
 (define (same-variable? v1 v2)
@@ -10,10 +31,10 @@
         (else (list '+ a1 a2))))
 
 (define (=number? exp num)
-  (and (numbder? exp) (= exp num)))
+  (and (number? exp) (= exp num)))
 
 (define (make-product m1 m2)
-  (cond ((or (=number? m1 0) (number? m2 0)) 0)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
         ((=number? m1 1) m2)
         ((=number? m2 1) m1)
         ((and (number? m1) (number? m2)) (* m1 m2))
@@ -27,8 +48,20 @@
 (define (augend s) (caddr s))
 
 (define (product? x)
-  (and (pair? x) (eq? (car d) '*)))
+  (and (pair? x) (eq? (car x) '*)))
 
 (define (multiplier p) (cadr p))
 
 (define (multiplicand p) (caddr p))
+
+(define (exponentiation? x)
+  (and (pair? x) (eq? (car x) '**)))
+
+(define (base e) (cadr e))
+
+(define (exponent e) (caddr e))
+
+(define (make-exponentiation base exponent)
+  (cond ((or (=number? base 0) (=number? exponent 0)) 0)
+        ((=number? exponent 1) base)
+        (else (list '** base exponent))))
