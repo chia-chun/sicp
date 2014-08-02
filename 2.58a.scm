703a833832824347e15ce3cdbc329d39ca95d76e
chia-chun <chia-chun@users.noreply.github.com>
2014-08-02 18:27:24 +0800

    2.58a

--- 2.58a.scm
+++ 2.58a.scm
@@ -28,7 +28,7 @@
   (cond ((=number? a1 0) a2)
         ((=number? a2 0) a1)
         ((and (number? a1) (number? a2)) (+ a1 a2))
-        (else (list '+ a1 a2))))
+        (else (list a1 '+ a2))))
 
 (define (=number? exp num)
   (and (number? exp) (= exp num)))
@@ -38,19 +38,19 @@
         ((=number? m1 1) m2)
         ((=number? m2 1) m1)
         ((and (number? m1) (number? m2)) (* m1 m2))
-        (else (list '* m1 m2))))
+        (else (list m1 '* m2))))
 
 (define (sum? x)
-  (and (pair? x) (eq? (car x) '+)))
+  (and (pair? x) (eq? (cadr x) '+)))
 
-(define (addend s) (cadr s))
+(define (addend s) (car s))
 
 (define (augend s) (caddr s))
 
 (define (product? x)
-  (and (pair? x) (eq? (car x) '*)))
+  (and (pair? x) (eq? (cadr x) '*)))
 
-(define (multiplier p) (cadr p))
+(define (multiplier p) (car p))
 
 (define (multiplicand p) (caddr p))
 
