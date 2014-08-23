1a73d9e48b73f8a91ed4df6038d8d0e6b52743bd
chia-chun <chia-chun@users.noreply.github.com>
2014-08-23 10:10:57 +0800

    2.65

---
+++ 2.65.scm
@@ -0,0 +1,68 @@
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+        (let ((left-result (partial-tree elts left-size)))
+          (let ((left-tree (car left-result))
+                (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            (let ((this-entry (car non-left-elts))
+                  (right-result (partial-tree (cdr non-left-elts) right-size)))
+              (let ((right-tree (car right-result))
+                    (remaining-elts (cdr right-result)))
+                (cons (make-tree this-entry left-tree right-tree)
+                      remaining-elts))))))))
+
+(define (make-tree entry left-branch right-branch)
+  (list entry left-branch right-branch))
+
+(define (tree-intersection-set tree1 tree2)
+  (list->tree (intersection-set (tree->list tree1) (tree->list tree2))))
+
+(define (tree-union-set tree1 tree2)
+  (list->tree (union-set (tree->list tree1) (tree->list tree2))))
+
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+        ((= x (car set)) #t)
+        ((< x (car set)) #f)
+        (else (element-of-set? x (cdr set)))))
+
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+      '()
+      (let ((x1 (car set1)) (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1
+                     (intersection-set (cdr set1)
+                                       (cdr set2))))
+              ((< x1 x2)
+               (intersection-set (cdr set1) set2))
+              ((< x2 x1)
+               (intersection-set set1 (cdr set2)))))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((= x (car set)) set)
+        ((< x (car set)) (cons x set))
+        (else (cons (car set) (adjoin-set x (cdr set))))))
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else (let ((x1 (car set1)) (x2 (car set2)))
+                (cond ((= x1 x2)
+                       (cons x1
+                             (union-set (cdr set1)
+                                        (cdr set2))))
+                      ((< x1 x2)
+                       (cons x1
+                             (union-set (cdr set1)
+                                        set2)))
+                      ((< x2 x1)
+                       (cons x2
+                             (union-set set1
+                                        (cdr set2)))))))))
