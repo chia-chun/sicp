fc4e70de0e5540c93eebf4b4a33ce93f6bf15f9c
chia-chun <chia-chun@users.noreply.github.com>
2014-10-01 16:31:04 +0800

    2.70

---
+++ 2.70.scm
@@ -0,0 +1,111 @@
+(define (generate-huffman-tree pairs)
+  (successive-merge (make-leaf-set pairs)))
+
+(define (successive-merge leaves)
+  (if (null? (cdr leaves))
+      (car leaves)
+      (successive-merge (adjoin-set (make-code-tree (car leaves)
+                                                    (cadr leaves))
+                                    (cddr leaves)))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set))) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+
+(define (encode message tree)
+  (if (null? message)
+      '()
+      (append (encode-symbol (car message) tree)
+              (encode (cdr message) tree))))
+
+(define (encode-symbol symbol tree)
+  (define (encode-symbol-true symbol tree)
+    (if (element-of-set? symbol (symbols (left-branch tree)))
+        (if (leaf? (left-branch tree))
+            '(0)
+            (cons 0 (encode-symbol-true symbol (left-branch tree))))
+        (if (leaf? (right-branch tree))
+            '(1)
+            (cons 1 (encode-symbol-true symbol (right-branch tree))))))
+  (if (element-of-set? symbol (symbols tree))
+      (encode-symbol-true symbol tree)
+      (error "The symbol is not in the tree")))
+
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+        ((equal? x (car set)) #t)
+        (else (element-of-set? x (cdr set)))))
+
+(define(make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+        (adjoin-set (make-leaf (car pair)   ;symbol
+                               (cadr pair)) ;frequency
+                    (make-leaf-set (cdr pairs))))))
+
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+
+(define (symbol-leaf x) (cadr x))
+
+(define (weight-leaf x) (caddr x))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+(define (left-branch tree) (car tree))
+
+(define (right-branch tree) (cadr tree))
+
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (cdr bits) tree))
+              (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+        ((= bit 1) (right-branch branch))
+        (else (error "bad bit - CHOOSE-BRANCH" bit))))
+
+(define rock-pairs
+  '((A 2) (BOOM 1) (GET 2) (JOB 2) (NA 16) (SHA 3) (YIP 9) (WAH 1)))
+
+(define sample-message '(GET A JOB
+                             SHA NA NA NA NA NA NA NA NA
+                             GET A JOB
+                             SHA NA NA NA NA NA NA NA NA
+                             WAH YIP YIP YIP YIP YIP YIP YIP YIP YIP
+                             SHA BOOM))
+
+;; scheme@(guile-user)> (define rock-tree (generate-huffman-tree rock-pairs))
+;; scheme@(guile-user)> rock-tree
+;; $2 = ((leaf NA 16) ((leaf YIP 9) (((leaf A 2) ((leaf WAH 1) (leaf BOOM 1) (WAH BOOM) 2) (A WAH BOOM) 4) ((leaf SHA 3) ((leaf JOB 2) (leaf GET 2) (JOB GET) 4) (SHA JOB GET) 7) (A WAH BOOM SHA JOB GET) 11) (YIP A WAH BOOM SHA JOB GET) 20) (NA YIP A WAH BOOM SHA JOB GET) 36)
+;; scheme@(guile-user)> (encode sample-message rock-tree)
+;; $3 = (1 1 1 1 1 1 1 0 0 1 1 1 1 0 1 1 1 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 0 0 1 1 1 1 0 1 1 1 0 0 0 0 0 0 0 0 0 1 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 1 1 0 1 1 0 1 1)
+;; 84 bits
+;; 36 words were encoded, so 108 bits would be needed using a fixed-length code.
