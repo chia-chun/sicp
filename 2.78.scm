a761f06813d9c622a97b70a1a49771acaf8ec67e
chia-chun <chia-chun@users.noreply.github.com>
2015-01-17 19:19:49 +0800

    2.78-revised

--- 2.78.scm
+++ 2.78.scm
@@ -1,6 +1,9 @@
 (load "generic-system.scm")
 
-(define attach-tag cons)
+(define (attach-tag tag datum)
+  (if (eq? tag 'scheme-number)
+      datum
+      (cons tag datum)))
 
 (define (type-tag datum)
   (cond ((pair? datum)
@@ -15,21 +18,3 @@
         ((number? datum)
          datum)
         (else (error "Bad typed datum -- CONTENTS" datum))))
-
-(define (install-scheme-number-package)
-  (put 'add '(scheme-number scheme-number)
-       (lambda (x y) (+ x y)))
-  (put 'sub '(scheme-number scheme-number)
-       (lambda (x y) (- x y)))
-  (put 'mul '(scheme-number scheme-number)
-       (lambda (x y) (* x y)))
-  (put 'div '(scheme-number scheme-number)
-       (lambda (x y) (/ x y)))
-  (put 'make 'scheme-number
-       (lambda (x) x))
-  'done)
-
-(install-scheme-number-package)
-
-(define (make-scheme-number n)
-  ((get 'make 'scheme-number) n))
