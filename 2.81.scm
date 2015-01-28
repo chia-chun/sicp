f4c9d45c40212ccb1515aef6747254e90e16dd1d
chia-chun <chia-chun@users.noreply.github.com>
2015-01-28 13:42:53 +0800

    2.81

--- 2.81.scm
+++ 2.81.scm
@@ -1,5 +1,4 @@
-(load "put-get.scm")
-(load "types.scm")
+(load "generic-system.scm")
 
 (define coercion-table (make-table))
 
@@ -11,6 +10,12 @@
 
 (put-coercion 'scheme-number 'complex scheme-number->complex)
 
+;; a. The program will enter an infinite loop.
+
+;; b. No. The coercion only happens when apply-generic could not find procedure
+;;    using (get op type-tags).
+
+;; c.
 (define (apply-generic op . args)
   (let ((type-tags (map type-tag args)))
     (let ((proc (get op type-tags)))
@@ -21,14 +26,17 @@
                     (type2 (cadr type-tags))
                     (a1 (car args))
                     (a2 (cadr args)))
-                (let ((t1->t2 (get-coercion type1 type2))
-                      (t2->t1 (get-coercion type2 type1)))
-                  (cond (t1->t2
-                         (apply-generic op (t1->t2 a1) a2))
-                        (t2->t1
-                         (apply-generic op a1 (t2->t1 a2)))
-                        (else
-                         (error "No method for these types"
-                                (list op type-tags))))))
+                (if (eqv? type1 type2)
+                    (error "No method for these types"
+                           (list op type-tags))
+                    (let ((t1->t2 (get-coercion type1 type2))
+                          (t2->t1 (get-coercion type2 type1)))
+                      (cond (t1->t2
+                             (apply-generic op (t1->t2 a1) a2))
+                            (t2->t1
+                             (apply-generic op a1 (t2->t1 a2)))
+                            (else
+                             (error "No method for these types"
+                                    (list op type-tags)))))))
               (error "No method for these types"
                      (list op type-tags)))))))
