f7caddf90e696d339a3de0dbc2b6ae99f16503de
chia-chun <chia-chun@users.noreply.github.com>
2015-06-07 12:41:44 +0800

    2.83

--- 2.83.scm
+++ 2.83.scm
@@ -1,8 +1,20 @@
-(define (raise-number integer)
-  (make-rational (contents integer) 1)))
+(load "generic-system.scm")
 
-(define (raise-rational rational)
-  (attach-tag 'real (/ (car (contents rational)) (cdr (contents rational)))))
+(define (install-raise)
+  (define (raise-number integer)
+    (make-rational integer 1))
 
-(define (raise-real real)
-  (make-complex-from-real-imag (contents real) 0))
+  (define (raise-rational rational)
+    (attach-tag 'real (/ (car rational) (cdr rational))))
+
+  (define (raise-real real)
+    (make-complex-from-real-imag real 0))
+
+  (put 'raise '(scheme-number) raise-number)
+  (put 'raise '(rational) raise-rational)
+  (put 'raise '(real) raise-real)
+  'done)
+
+(install-raise)
+
+(define (raise x) (apply-generic 'raise x))
